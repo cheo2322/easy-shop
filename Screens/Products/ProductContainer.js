b7343f061d544a220eaf1a8f143e8daf5ae134a6
Cheo <barcecheo2319@gmail.com>
2022-07-20 01:25:57 -0500

    listing products

--- Screens/Products/ProductContainer.js
+++ Screens/Products/ProductContainer.js
@@ -1,10 +1,39 @@
-import React, { useState } from "react";
-import { View, Text, StyleSheet, ActivityIndicator } from "react-native";
+import React, { useState, useEffect } from 'react';
+import { View, Text, StyleSheet, ActivityIndicator, FlatList } from 'react-native';
+
+import ProductList from './ProductList';
+
+const data = require('../../assets/data/products.json');
 
 const ProductContainer = () => {
+
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    setProducts(data);
+
+    return () => {
+      setProducts([]);
+    }
+  }, []);
+
   return (
     <View>
-      <Text></Text>
+      <Text>Product Container</Text>
+      <View style={{ marginTop: 100 }}>
+        <FlatList
+          numColumns={2}
+          data={products}
+          renderItem={({ item }) =>
+            <ProductList
+              key={item.id}
+              item={item}
+            />}
+          keyExtractor={item => item.name}
+        />
+      </View>
     </View>
   );
-};
+}
+
+export default ProductContainer;
