29c32a53786a4d70fa65af62bf6775b5d300098a
Cheo <barcecheo2319@gmail.com>
2022-07-31 02:51:53 -0500

    cart details

--- Navigators/Main.js
+++ Navigators/Main.js
@@ -7,6 +7,8 @@ import Icon from "react-native-vector-icons/FontAwesome";
 import HomeNavigator from "./HomeNavigator";
 import CartNavigator from "./CartNavigator";
 
+import CartIcon from "../Shared/CartIcon";
+
 const Tab = createBottomTabNavigator();
 
 const Main = () => {
@@ -17,6 +19,7 @@ const Main = () => {
         keyboardHidesTabBar: true,
         showLabel: false,
         activeTintColor: "#e91e63",
+        headerShown: false,
       }}
     >
       <Tab.Screen
@@ -34,7 +37,10 @@ const Main = () => {
         component={CartNavigator}
         options={{
           tabBarIcon: ({ color }) => (
-            <Icon name="shopping-cart" color={color} size={30} />
+            <View>
+              <Icon name="shopping-cart" color={color} size={30} />
+              <CartIcon />
+            </View>
           ),
         }}
       />
