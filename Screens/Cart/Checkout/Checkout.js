9537c1bb546e850da2247841f0695668431f63dd
Cheo <barcecheo2319@gmail.com>
2022-07-31 21:10:30 -0500

    checkout first approach

--- Screens/Cart/Checkout/Checkout.js
+++ Screens/Cart/Checkout/Checkout.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Text } from "react-native";
 
 const Checkout = () => {
-  <Text>CHECKOUT WORKS</Text>;
+  return <Text>CHECKOUT WORKS</Text>;
 };
 
 export default Checkout;
