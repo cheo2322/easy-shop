171a339c33ef1b8b9c31a13f002693309e97607b
Cheo <barcecheo2319@gmail.com>
2022-08-08 23:33:35 -0500

    products admin

--- Screens/Admin/Products.js
+++ Screens/Admin/Products.js
@@ -1,12 +1,112 @@
-import React from "react";
-import { View, Text } from "react-native";
+import React, { useState, useCallback } from "react";
+import {
+  View,
+  Text,
+  FlatList,
+  ActivityIndicator,
+  StyleSheet,
+  Dimensions,
+} from "react-native";
+import { Header, Item, Input } from "native-base";
+import Icon from "react-native-vector-icons/FontAwesome";
+
+import { useFocusEffect } from "@react-navigation/native";
+import axios from "axios";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+import baseURL from "../../assets/common/baseUrl";
+
+var { width, height } = Dimensions.get("window");
 
 const Products = (props) => {
+  const [productList, setProductList] = useState();
+  const [productFilter, setProductFilter] = useState();
+  const [loading, setLoading] = useState(true);
+  const [token, setToken] = useState();
+
+  useFocusEffect(
+    useCallback(() => {
+      // Get Token
+      AsyncStorage.getItem("jwt")
+        .then((res) => {
+          setToken(res);
+        })
+        .catch((error) => console.log(error));
+
+      axios
+        .get(`${baseURL}products`)
+        .then((res) => {
+          setProductList(res.data);
+          setProductFilter(res.data);
+          setLoading(false);
+        })
+        .catch((error) => console.log(error));
+
+      return () => {
+        setProductList();
+        setProductFilter();
+        setLoading(true);
+      };
+    }, [])
+  );
+
   return (
     <View>
-      <Text>Products Screen</Text>
+      <View>
+        <Header searchBar rounded>
+          <Item style={{ padding: 5 }}>
+            <Icon name="search" />
+            <Input
+              placeholder="Search"
+              // onChange
+            />
+          </Item>
+        </Header>
+      </View>
+
+      {loading ? (
+        <View>
+          <ActivityIndicator size="large" color="red" />
+        </View>
+      ) : (
+        <FlatList
+          data={productFilter}
+          renderItem={({ item, index }) => <Text>{item.name}</Text>}
+          keyExtractor={(item) => item.id}
+        />
+      )}
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  listHeader: {
+    flexDirection: "row",
+    padding: 5,
+    backgroundColor: "gainsboro",
+  },
+  headerItem: {
+    margin: 3,
+    width: width / 6,
+  },
+  spinner: {
+    height: height / 2,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  container: {
+    marginBottom: 160,
+    backgroundColor: "white",
+  },
+  buttonContainer: {
+    margin: 20,
+    alignSelf: "center",
+    flexDirection: "row",
+  },
+  buttonText: {
+    marginLeft: 4,
+    color: "white",
+  },
+});
 
 export default Products;
