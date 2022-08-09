b61b05fffb47b8fdf5edf3e378bef0670cc58419
Cheo <barcecheo2319@gmail.com>
2022-08-08 21:12:05 -0500

    adding context to login

--- Screens/User/Login.js
+++ Screens/User/Login.js
@@ -1,14 +1,25 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useContext, useState } from "react";
 import { View, Text, StyleSheet, Button } from "react-native";
 
 import FormContainer from "../../Shared/Form/FormContainer";
 import Input from "../../Shared/Form/Input";
 import Error from "../../Shared/Form/Error";
 
+// Context
+import AuthGlobal from "../../Context/store/AuthGlobal";
+import { loginUser } from "../../Context/actions/Auth.actions";
+
 const Login = (props) => {
+  const context = useContext(AuthGlobal);
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState("");
+
+  useEffect(() => {
+    if (context.stateUser.isAuthenticated === true) {
+      props.navigation.navigate("UserProfile");
+    }
+  }, [context.stateUser.isAuthenticated]);
 
   const handleSubmit = () => {
     const user = {
@@ -19,6 +30,8 @@ const Login = (props) => {
     if (email === "" || password === "") {
       setError("Please, fill in your credentials");
     } else {
+      loginUser(user, context.dispatch);
+
       console.log("Login success!");
     }
   };
