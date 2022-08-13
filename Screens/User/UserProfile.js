8fb650cd09ccb1f9d21b620db83c243df6575591
Cheo <barcecheo2319@gmail.com>
2022-08-13 15:24:56 -0500

    formatting

--- Screens/User/UserProfile.js
+++ Screens/User/UserProfile.js
@@ -1,14 +1,14 @@
-import React, { useContext, useState, useCallback, useEffect } from "react";
-import { View, Text, ScrollView, StyleSheet, Button } from "react-native";
-import { Container } from "native-base";
-import { useFocusEffect } from "@react-navigation/native";
-import AsyncStorage from "@react-native-async-storage/async-storage";
+import React, { useContext, useState, useCallback, useEffect } from 'react';
+import { View, Text, ScrollView, StyleSheet, Button } from 'react-native';
+import { Container } from 'native-base';
+import { useFocusEffect } from '@react-navigation/native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
-import axios from "axios";
+import axios from 'axios';
 
-import baseURL from "../../assets/common/baseUrl";
-import AuthGlobal from "../../Context/store/AuthGlobal";
-import { logoutUser } from "../../Context/actions/Auth.actions";
+import baseURL from '../../assets/common/baseUrl';
+import AuthGlobal from '../../Context/store/AuthGlobal';
+import { logoutUser } from '../../Context/actions/Auth.actions';
 
 const UserProfile = (props) => {
   const context = useContext(AuthGlobal);
@@ -19,10 +19,10 @@ const UserProfile = (props) => {
       context.stateUser.isAuthenticated === false ||
       context.stateUser.isAuthenticated === null
     ) {
-      props.navigation.navigate("Login");
+      props.navigation.navigate('Login');
     }
 
-    AsyncStorage.getItem("jwt")
+    AsyncStorage.getItem('jwt')
       .then((res) => {
         axios
           .get(`${baseURL}users/${context.stateUser.user.userId}`, {
@@ -43,21 +43,21 @@ const UserProfile = (props) => {
     <Container style={styles.container}>
       <ScrollView contentContainerStyle={styles.subContainer}>
         <Text style={{ fontSize: 30 }}>
-          {userProfile ? userProfile.name : ""}
+          {userProfile ? userProfile.name : ''}
         </Text>
         <View style={{ marginTop: 20 }}>
           <Text style={{ margin: 10 }}>
-            Email: {userProfile ? userProfile.email : ""}
+            Email: {userProfile ? userProfile.email : ''}
           </Text>
           <Text style={{ margin: 10 }}>
-            Phone: {userProfile ? userProfile.phone : ""}
+            Phone: {userProfile ? userProfile.phone : ''}
           </Text>
         </View>
         <View style={{ marginTop: 80 }}>
           <Button
-            title={"Sign out"}
+            title={'Sign out'}
             onPress={() => [
-              AsyncStorage.removeItem("jwt"),
+              AsyncStorage.removeItem('jwt'),
               logoutUser(context.dispatch),
             ]}
           />
@@ -70,15 +70,15 @@ const UserProfile = (props) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: "center",
+    alignItems: 'center',
   },
   subContainer: {
-    alignItems: "center",
+    alignItems: 'center',
     marginTop: 60,
   },
   order: {
     marginTop: 20,
-    alignItems: "center",
+    alignItems: 'center',
     marginBottom: 60,
   },
 });
